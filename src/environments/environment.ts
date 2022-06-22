0e9e7dc65c057e8b061481a12a62d39535fb858f
shreyaskg <44202825+shreyaskg@users.noreply.github.com>
2022-06-22 13:56:35 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,5 @@
 import { initializeApp } from "firebase/app";
 
 export const firebaseConfig = {
-  production:false,
-  firebase: {
-    projectId: 'parkingsystem-946bf',
-    appId: '1:41429715745:web:b32bf1a7f76257dd66cc60',
-    storageBucket: 'parkingsystem-946bf.appspot.com',
-    locationId: 'us-central',
-    apiKey: 'AIzaSyCnSmV9JgTKpkDsOM_AG1o7dX6wlCKDTjo',
-    authDomain: 'parkingsystem-946bf.firebaseapp.com',
-    messagingSenderId: '41429715745',
-    measurementId: 'G-478F9R7XY7',
-  }
 };
 
